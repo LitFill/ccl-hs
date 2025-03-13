8a3211b75a7c67154b0ca5eecf5c742808b5d359
LitFill <marrazzy54@gmail.com>
2025-03-13 23:25:34 +0700

    refactor: attempt to fix nested entries

--- src/CCL.hs
+++ src/CCL.hs
@@ -15,8 +15,13 @@ import Text.Megaparsec.Char.Lexer qualified as L
 ----------------------------------------
 
 data KV where
-    KV :: {key :: String, value :: String}
+    KV :: {key :: String, value :: Value}
         -> KV
+    deriving (Show, Eq)
+
+data Value where
+    Simple :: String -> Value
+    Block  :: [KV]   -> Value
     deriving (Show, Eq)
 
 ----------------------------------------
@@ -26,7 +31,8 @@ data KV where
 type Parser = Parsec Void Text
 
 sc :: Parser ()
-sc = L.space space1 (L.skipLineComment "/=") Text.Megaparsec.empty
+sc = L.space space1 empty empty
+-- sc = L.space space1 (L.skipLineComment "/=") empty
 
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme sc
@@ -40,22 +46,25 @@ eqP = symbol "="
 strip :: String -> String
 strip = T.unpack . T.strip . T.pack
 
+emptyP :: Parser String
+emptyP = T.unpack <$> string ""
+
 keyP :: Parser String
 keyP = strip <$>
-    lexeme (many (noneOf ("=\n":: String))) <?> "key"
+    lexeme (many (noneOf (" =":: String))) <?> "key"
 
 keyStart :: Parser ()
-keyStart = do
-    _ <- sc
-    _ <- many (noneOf ['=', '\n'])
+keyStart = label "key-start" $ do
+    _ <- sc -- optional (some spaceChar)
+    _ <- many (noneOf ['='])
     _ <- eqP
     pure ()
 
 lineContent :: Parser String
-lineContent = many (noneOf ['\n'])
+lineContent = many (noneOf ['\n']) <?> "line-content"
 
 valueP :: Parser String
-valueP = do
+valueP = label "value" $ do
     _         <- eqP
     fstLine   <- lineContent
     restLines <- many . try $ do
@@ -68,14 +77,44 @@ valueP = do
         T.intercalate "\n" $
         T.pack <$> (fstLine : restLines)
 
-kvP :: Parser KV
-kvP = do
+restLinesF :: Parser String
+restLinesF = label "restlinefn" $ do
+    ls <- many . try $ do
+        _ <- newline
+        notFollowedBy (try keyStart)
+        lineContent
+    pure $ concat ls
+
+-- kvP :: Parser KV
+-- kvP = label "key value pair" $ do
+--     k <- keyP
+--     v <- valueP
+--     pure $ KV k v
+
+kvP' :: Parser KV
+kvP' = label "key value pair" $ do
+    indentPos <- L.indentLevel
     k <- keyP
-    v <- valueP
-    pure $ KV k v
+    _ <- eqP
+    space
+    v <- strip <$> manyTill anySingle newline
+    if null v then do
+        block <- many $ do
+            _ <- L.indentGuard space GT indentPos
+            kvP'
+        pure $ KV k (Block block)
+    else
+        pure $ KV k (Simple v)
 
 configP :: Parser [KV]
-configP = kvP `sepEndBy` newline <* eof
+configP = label "config" $ do kvP' `sepBy` newline <* eof
+
+config' :: Parser [KV]
+config' = many kvP' <* eof
+
+----------------------------------------
+-- samples
+----------------------------------------
 
 sample :: Text
 sample = T.pack $
@@ -84,4 +123,22 @@ sample = T.pack $
         , "name = LitFill The Sanct"
         , "createdAt = 2025-03-13"
         , "timeSpentImplementing = 2d"
+        ]
+
+emptyKeys :: Text
+emptyKeys = T.pack $
+    unlines
+        [ "= one"
+        , "= two"
+        , "= three"
+        , "= four"
+        ]
+
+emptyVals :: Text
+emptyVals = T.pack $
+    unlines
+        [ "one ="
+        , "two ="
+        , "three ="
+        , "four ="
         ]
